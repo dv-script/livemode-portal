5965522218e4e1a30e92a29d84517a1adbcffa1d
Davi Hasson Castro <davihasson@gmail.com>
2023-12-27 23:38:30 -0300

    feat: adding a form to login page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Link from 'next/link';
+import { useFormState } from "react-dom";
+import { authenticateUser } from '@/actions/authenticateUser';
 
-export function LoginForm() {
+export async function LoginForm() {
+  const [state, dispatch] = useFormState(authenticateUser, undefined);
+
   return (
-    <form className="z-10 max-w-lg p-8 m-4 bg-white text-gray-800 border border-gray-200 rounded-lg flex flex-col justify-center items-center gap-8 md:p-4 md:gap-4">
+    <form action={dispatch} className="z-10 max-w-lg p-8 m-4 bg-white text-gray-800 border border-gray-200 rounded-lg flex flex-col justify-center items-center gap-8 md:p-4 md:gap-4">
       <h1 className="text-[1.25rem] font-semibold">Livemode Single-Sign-On Portal</h1>
       <p className="text-sm">
         This is the general log-in page for your Bundesliga content. After your
@@ -12,15 +17,15 @@ export function LoginForm() {
       <div className="w-full flex flex-col gap-4">
         <div className="flex flex-col gap-2">
           <label className="text-sm">E-mail</label>
-          <input type="email" placeholder="E-mail address" className="w-full p-2 border border-gray-300 bg-gray-100 text-gray-800 rounded-sm focus:outline-none" />
+          <input name='email' type="email" placeholder="E-mail address" className="w-full p-2 border border-gray-300 bg-gray-100 text-gray-800 rounded-sm focus:outline-none" />
         </div>
 
         <div className="flex flex-col gap-2">
           <label className="text-sm">Password</label>
-          <input type="password" placeholder="Password" className="w-full p-2 border border-gray-300 bg-gray-100 text-gray-800 rounded-sm focus:outline-none" />
+          <input name='password' type="password" placeholder="Password" className="w-full p-2 border border-gray-300 bg-gray-100 text-gray-800 rounded-sm focus:outline-none" />
         </div>
       </div>
-      
+
       <button className="w-full p-2 bg-green-500 text-white rounded-sm transition-opacity duration-200 hover:opacity-80">Sign in</button>
 
       <div className="w-full flex flex-col gap-1 items-center">
