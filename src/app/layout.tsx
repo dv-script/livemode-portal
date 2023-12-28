5a37a53321e8bcb15563edb38145b4f079de2994
Davi Hasson Castro <davihasson@gmail.com>
2023-12-27 23:35:24 -0300

    chore: adding favicon to head layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import { Poppins } from 'next/font/google'
 import '@/styles/globals.css'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -19,7 +22,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <Head>
+        <link rel="icon" href="./favicon.png" />
+      </Head>
+      <body className={poppins.className}>
+        <div className="flex flex-col min-h-screen" >
+          <Header />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }
