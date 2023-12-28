813979e3cffd20e4ff6ed7bac31a3083dcf1b08d
Davi Hasson Castro <davihasson@gmail.com>
2023-12-27 23:36:00 -0300

    style: adding style to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div>
+        <div className="flex justify-center items-center h-[calc(100vh-156px)]">
             Protected page
         </div>
     )
