e0595db0854fa5d8439599b49e0f2038118034cf
Davi Hasson Castro <davihasson@gmail.com>
2023-12-31 11:57:09 -0300

    feat: adding new features to specified roles and logout button after sign-in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,34 @@
+import { auth } from "@/app/auth/providers";
+import { PiQuestion, PiLockKey } from 'react-icons/pi';
 import logoImage from '../assets/logoImage.png';
-import { PiQuestion } from 'react-icons/pi';
 import Image from 'next/image';
 import Link from 'next/link';
+import { UserLogout } from "./UserLogout";
 
-export function Header() {
+export async function Header() {
+    const session = await auth();
+
     return (
         <header className="w-full h-16 bg-white">
             <div className="h-full max-w-[1300px] mx-auto bg-white text-gray-800 flex justify-between items-center gap-8 px-4">
                 <Link href="/" className="flex items-center justify-center h-full">
-                    <Image src={logoImage} alt='Livemode Logo' className="h-12 object-contain" />
+                    <Image src={logoImage} alt='Livemode Logo' className="h-12 object-contain w-auto" priority />
                 </Link>
 
                 <div className="flex items-center justify-center h-full gap-8">
+                    {session?.user.roles.includes('admin') && (
+                        <Link href="/admin" className="flex items-center gap-2 h-full text-gray-800 font-bold relative cursor-pointer hover:text-green-500 user-button">
+                            <PiLockKey size={26} />
+                            <span className="whitespace-nowrap text-sm leading-7 hidden md:block">Admin</span>
+                        </Link>
+                    )}
                     <Link href="/" className="flex items-center gap-2 h-full text-gray-800 font-bold relative cursor-pointer hover:text-green-500 user-button">
                         <PiQuestion size={26} />
                         <span className="whitespace-nowrap text-sm leading-7 hidden md:block">Help & Contact</span>
                     </Link>
+                    {session?.user && (
+                        <UserLogout />
+                    )}
                 </div>
             </div>
         </header>
