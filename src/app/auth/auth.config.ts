081af929a6fd408afec656775ce3c0eba7e18453
Davi Hasson Castro <davihasson@gmail.com>
2023-12-31 11:52:35 -0300

    chore: adding new admin route system and fixing the logical from privateRoutes

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -3,23 +3,34 @@ import { NextAuthConfig } from "next-auth";
 export const authConfig = {
     providers: [],
     pages: {
-        signIn: '/auth/sign-in'
+        signIn: '/auth/sign-in',
     },
     callbacks: {
         authorized: ({ auth, request: { nextUrl } }) => {
             const isLogIn = !!auth?.user;
-            const isPrivateRoutes = nextUrl.pathname.startsWith('/');
             const isAuthRoutes = nextUrl.pathname.startsWith('/auth');
+            const isPrivateRoutes = !isAuthRoutes && nextUrl.pathname.startsWith('/');
+
+            const isAdmin = auth?.user?.roles?.includes('admin');
+            const isAdminRoutes = nextUrl.pathname.startsWith('/admin');
 
             if (!isLogIn && isPrivateRoutes) {
                 return false;
             }
 
-            if (isLogIn && isAuthRoutes) {
+            if (isLogIn && isAuthRoutes || isAdminRoutes && !isAdmin) {
                 return Response.redirect(new URL('/', nextUrl));
             }
 
             return true
+        },
+        jwt({ token, user }) {
+            if (user) token.roles = user.roles
+            return token;
+        },
+        session({ session, token }) {
+            if (token.roles) session.user.roles = token.roles
+            return session;
         }
     }
 } as NextAuthConfig;
