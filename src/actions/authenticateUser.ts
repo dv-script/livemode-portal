d87541fcb73da25f8c92978a7563aa553ac0c2ae
Davi Hasson Castro <davihasson@gmail.com>
2023-12-31 11:51:00 -0300

    chore: adding redirect after trying authenticate the user

--- src/actions/authenticateUser.ts
+++ src/actions/authenticateUser.ts
@@ -1,17 +1,19 @@
 "use server"
 import { signIn } from "@/app/auth/providers";
+import { redirect } from "next/navigation";
 
 export async function authenticateUser(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      await signIn('credentials', Object.fromEntries(formData));
-    } catch (error) {
-      if ((error as Error).message.includes("CredentialsSignin")) {
-        return "CredentialsSignin";
-      }
-
-      throw error;
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+    redirect('/');
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialsSignIn";
     }
+
+    throw error;
   }
+}
