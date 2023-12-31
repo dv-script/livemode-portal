a59e1f4fb04554079daeea2cab463b466a39d489
Davi Hasson Castro <davihasson@gmail.com>
2023-12-31 11:50:01 -0300

    style: changing the style from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-[calc(100vh-156px)]">
-            Protected page
+        <div className="flex gap-4 justify-center items-center p-4 h-[calc(100vh-156px)]">
+            <h1 className="text-white">Protected Page</h1>
         </div>
     )
 }
