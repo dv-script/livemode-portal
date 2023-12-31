bb2938edd2d9c49af1256bac38d3f71db0d283d4
Davi Hasson Castro <davihasson@gmail.com>
2023-12-31 11:52:48 -0300

    feat: new admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Admin() {
+    return (
+        <div className="flex items-center p-4">
+            <Link href="/admin/add-a-new-user" className="bg-green-500 text-white px-4 py-2 rounded-lg transition-colors hover:bg-green-600">
+                Add a new user
+            </Link>
+        </div>
+    )
+}
