443bcce06d71b2826627af39775ec7523ed11f03
Davi Hasson Castro <davihasson@gmail.com>
2024-01-01 16:36:37 -0300

    chore: adding all cards to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Card } from "@/components/Card";
+import { servicesInfoMap } from "@/enum/services";
+
 export default function Home() {
-	return (
-		<div className="flex gap-4 justify-center items-center p-4 h-[calc(100vh-156px)]">
-			<h1 className="text-white">Protected Page</h1>
-		</div>
-	)
+    return (
+        <div className="flex gap-4 justify-center items-center flex-wrap px-4 py-28 min-h-screen background">
+            {Object.values(servicesInfoMap).map((serviceInfo) => (
+                <Card
+                    key={serviceInfo.id}
+                    title={serviceInfo.title}
+                    description={serviceInfo.description}
+                    image={serviceInfo.image}
+                    link={serviceInfo.link}
+                />
+            ))}
+        </div>
+    )
 }
