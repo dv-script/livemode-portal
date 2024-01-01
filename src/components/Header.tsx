ae4549da8c8e7a7d31d99923ae6c603827403fac
Davi Hasson Castro <davihasson@gmail.com>
2024-01-01 16:37:54 -0300

    style: adding z-index and changing the gap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,13 @@ export async function Header() {
     const session = await auth();
 
     return (
-        <header className="w-full h-16 bg-zinc-900 bg-opacity-80 fixed">
+        <header className="w-full h-16 bg-zinc-900 bg-opacity-80 fixed z-50">
             <div className="h-full max-w-[1300px] mx-auto flex justify-between items-center gap-8 px-4">
                 <Link href="/" className="flex items-center justify-center h-full">
                     <Image src={logoImage} alt='Livemode Logo' className="h-12 object-contain w-auto" priority />
                 </Link>
 
-                <div className="flex items-center justify-center h-full gap-8">
+                <div className="flex items-center justify-center h-full gap-4">
                     {session?.user.roles.includes('admin') && (
                         <Link href="/admin" className="flex items-center gap-2 h-full text-zinc-100 relative cursor-pointer hover:text-green-500 user-button">
                             <PiLockKey size={26} />
