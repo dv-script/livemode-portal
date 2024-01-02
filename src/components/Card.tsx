9485884fce5f6e33c9d53aac7bd0164991153446
Davi Hasson Castro <davihasson@gmail.com>
2024-01-02 00:30:05 -0300

    refactor: refactoring the code and adding new features with nextui

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,35 +1,49 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { PiQuestion } from 'react-icons/pi'
+import Image from "next/image";
+import Link from "next/link";
+import { PiQuestionFill } from "react-icons/pi";
+import { PopoverDescriptionService } from "./PopoverDescriptionService";
 
 interface CardProps {
   title: string
+  small_description: string
   description: string
+  description_title: string
   image: string
   link: string
 }
 
-export function Card({ title, description, image, link }: CardProps) {
+export function Card({
+  title,
+  small_description,
+  description,
+  description_title,
+  image,
+  link,
+}: CardProps) {
   return (
-    <Link href={link} className="group flex flex-col max-w-72 h-96 bg-white rounded-lg overflow-hidden">
-      <div className="w-full h-60 overflow-hidden">
+    <div className="flex flex-col max-w-72 h-96 bg-white rounded overflow-hidden">
+      <Link href={link} className="w-full h-52 overflow-hidden">
         <Image
           src={image}
           alt={`Image from ${title}`}
           width={500}
           height={500}
-          className="object-cover bg-cover bg-no-repeat bg-center w-full h-full group-hover:scale-110 duration-300 ease-in-out -z-10"
+          className="object-cover bg-cover bg-no-repeat bg-center w-full h-full hover:scale-110 duration-300 ease-in-out -z-10"
         />
-      </div>
-      <div className="flex flex-col gap-1 p-4">
+      </Link>
+      <div className="flex flex-col gap-1 p-4 flex-1">
         <div className="flex items-center gap-2">
-          <h2 className="text-lg">{title}</h2>
-          <button className="hover:text-green-500">
-            <PiQuestion size={16} />
-          </button>
+          <Link href={link} className="text-lg hover:underline">
+            {title}
+          </Link>
+          <PopoverDescriptionService description_title={description_title} description={description}>
+              <button className="p-1 rounded-sm text-zinc-500 outline-none hover:bg-zinc-200">
+                <PiQuestionFill size={20} />
+              </button>
+          </PopoverDescriptionService>
         </div>
-        <p className="text-zinc-500 text-sm">{description}</p>
+        <p className="text-zinc-500 text-sm">{small_description}</p>
       </div>
-    </Link>
-  )
+    </div>
+  );
 }
