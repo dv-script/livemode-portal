353d4d88b05e03b55fa4a8b0cd04b532b119b23a
Davi Hasson Castro <davihasson@gmail.com>
2024-01-02 00:27:38 -0300

    chore: giving more parameters to card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,20 @@ import { servicesInfoMap } from "@/enum/services";
 
 export default function Home() {
     return (
-        <div className="flex gap-4 justify-center items-center flex-wrap px-4 py-28 min-h-screen background">
-            {Object.values(servicesInfoMap).map((serviceInfo) => (
-                <Card
-                    key={serviceInfo.id}
-                    title={serviceInfo.title}
-                    description={serviceInfo.description}
-                    image={serviceInfo.image}
-                    link={serviceInfo.link}
-                />
-            ))}
+        <div className="min-h-screen background">
+            <div className="flex gap-4 justify-center items-center flex-wrap px-4 py-28 m-auto max-w-[1300px]">
+                {Object.values(servicesInfoMap).map((serviceInfo) => (
+                    <Card
+                        key={serviceInfo.id}
+                        title={serviceInfo.title}
+                        small_description={serviceInfo.small_description}
+                        description_title={serviceInfo.description_title}
+                        description={serviceInfo.description}
+                        image={serviceInfo.image}
+                        link={serviceInfo.link}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
