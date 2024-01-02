be51e0c9a60668e0287baeea5dfdfd209d5d6983
Davi Hasson Castro <davihasson@gmail.com>
2024-01-02 00:31:03 -0300

    refactor: refactoring the header and adding new funcionabilties with nextui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,38 @@
+import Link from "next/link";
 import { auth } from "@/app/auth/providers";
-import { PiQuestion, PiLockKey } from 'react-icons/pi';
-import logoImage from '../assets/logoImage.png';
-import Image from 'next/image';
-import Link from 'next/link';
-import { UserLogout } from "./UserLogout";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button} from "@nextui-org/react";
+import { UserProfile } from "./UserProfile"
 
 export async function Header() {
-    const session = await auth();
+  const session = await auth()
 
-    return (
-        <header className="w-full h-16 bg-zinc-900 bg-opacity-80 fixed z-50">
-            <div className="h-full max-w-[1300px] mx-auto flex justify-between items-center gap-8 px-4">
-                <Link href="/" className="flex items-center justify-center h-full">
-                    <Image src={logoImage} alt='Livemode Logo' className="h-12 object-contain w-auto" priority />
-                </Link>
-
-                <div className="flex items-center justify-center h-full gap-4">
-                    {session?.user.roles.includes('admin') && (
-                        <Link href="/admin" className="flex items-center gap-2 h-full text-zinc-100 relative cursor-pointer hover:text-green-500 user-button">
-                            <PiLockKey size={26} />
-                            <span className="whitespace-nowrap text-sm leading-7 hidden md:block">Admin</span>
-                        </Link>
-                    )}
-                    <Link href="/" className="flex items-center gap-2 h-full text-zinc-100 relative cursor-pointer hover:text-green-500 user-button">
-                        <PiQuestion size={26} />
-                        <span className="whitespace-nowrap text-sm leading-7 hidden md:block">Help & Contact</span>
-                    </Link>
-                    {session?.user && (
-                        <UserLogout />
-                    )}
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Link href="/" className="font-bold text-inherit">LIVEMODE</Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        {session?.user ? (
+            <UserProfile 
+              first_name={session?.user?.first_name}
+              last_name={session?.user?.last_name}
+              company={session?.user?.company}
+              isAdmin={session?.user?.roles.includes('admin') && true}
+            />
+        ) : (
+          <>
+            <NavbarItem className="hidden lg:flex">
+              <Link href="/">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="success" href="#" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </>
+        )}
+      </NavbarContent>
+    </Navbar>
+  );
 }
+
