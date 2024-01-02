3b9a7be6ad3cccc5e151b27606c148720622a58f
Davi Hasson Castro <davihasson@gmail.com>
2024-01-02 00:27:12 -0300

    chore: adding a nextui provider to layout and changing the style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,40 @@
-import type { Metadata } from 'next'
-import Head from 'next/head'
-import { Poppins } from 'next/font/google'
-import '@/styles/globals.css'
-import { Header } from '@/components/Header'
-import { Footer } from '@/components/Footer'
+import type { Metadata } from "next";
+import Head from "next/head";
+import { Poppins } from "next/font/google";
+import "@/styles/globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { NextUiProvider } from "@/providers/next-ui";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'Livemode Portal',
-  description: 'Livemode Portal'
-}
+  title: "Livemode Portal",
+  description: "Livemode Portal",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <Head>
         <link rel="icon" href="./favicon.png" />
       </Head>
-      <body className={`${poppins.className} dark:bg-zinc-900 bg-zinc-100`}>
-        <div className="flex flex-col min-h-screen" >
-          <Header />
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer />
-        </div>
+      <body className={`${poppins.className}`}>
+        <NextUiProvider>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </NextUiProvider>
       </body>
     </html>
-  )
+  );
 }
