1089ff2c8179f5a0198d9d0ce249ef7a8eeccaaf
Davi Hasson Castro <davihasson@gmail.com>
2024-01-02 00:28:09 -0300

    style: just adding new styles to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Admin() {
     return (
-        <div className="flex items-center py-20 px-4">
-            <Link href="/admin/add-a-new-user" className="bg-green-500 text-white px-4 py-2 rounded-lg transition-colors hover:bg-green-600">
-                Add a new user
-            </Link>
+        <div className="flex items-center justify-center py-20 px-4 min-h-screen background">
+            <div className="bg-white px-24 py-52 flex flex-col rounded-lg">
+                <Link href="/admin/add-a-new-user" className="bg-green-500 text-white px-4 py-2 rounded-lg transition-colors hover:bg-green-600">
+                    Add a new user
+                </Link>
+            </div>
         </div>
     )
 }
