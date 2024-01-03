79d5579d2b085a2e886650e6d906bcce75782559
Davi Hasson Castro <davihasson@gmail.com>
2024-01-03 01:15:00 -0300

    style: chaging the style and adding users table component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { UsersTable } from "@/components/users-table";
 
 export default function Admin() {
     return (
-        <div className="flex items-center justify-center py-20 px-4 min-h-screen background">
-            <div className="bg-white px-24 py-52 flex flex-col rounded-lg">
-                <Link href="/admin/add-a-new-user" className="bg-green-500 text-white px-4 py-2 rounded-lg transition-colors hover:bg-green-600">
-                    Add a new user
-                </Link>
+        <div className="flex p-4 min-h-screen background">
+            <div className="mx-auto flex flex-col gap-4 max-w-[1300px]">
+                <div className="bg-white flex flex-col rounded-lg p-3 w-full lg:w-1/2">
+                    <div className="flex gap-4 items-center">
+                    <Link href="/admin/add-a-new-user" className="bg-green-500 w-fit text-white px-4 py-2 rounded-full transition-colors hover:bg-green-600">
+                        +
+                    </Link>
+                    <h2>Users</h2>
+                    </div>
+                    <div className="flex flex-col mt-4 overflow-x-auto">
+                        <UsersTable />
+                    </div>
+                </div>
             </div>
         </div>
     )
