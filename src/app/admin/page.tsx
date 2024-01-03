c795f061e28f348553b46acd45b7c7de4b1a86b3
Davi Hasson Castro <davihasson@gmail.com>
2024-01-03 16:49:10 -0300

    style: adding resposibility to admin page and overflow

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,8 +4,8 @@ import { UsersTable } from "@/components/users-table";
 export default function Admin() {
     return (
         <div className="flex p-4 min-h-screen background">
-            <div className="mx-auto flex flex-col gap-4 max-w-[1300px]">
-                <div className="bg-white flex flex-col rounded-lg p-3 w-full lg:w-1/2">
+            <div className="mx-auto flex flex-col gap-4 max-w-[1300px] overflow-hidden">
+                <div className="bg-white flex flex-col rounded-lg p-3 w-full h-full lg:h-1/2">
                     <div className="flex gap-4 items-center">
                     <Link href="/admin/add-a-new-user" className="bg-green-500 w-fit text-white px-4 py-2 rounded-full transition-colors hover:bg-green-600">
                         +
