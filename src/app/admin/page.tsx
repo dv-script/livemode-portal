452f26f8382a7a238d8b811bf872c891bc55afbd
Davi Hasson Castro <davihasson@gmail.com>
2024-01-04 00:56:18 -0300

    style: linting the code

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,21 +2,24 @@ import Link from "next/link";
 import { UsersTable } from "@/components/users-table";
 
 export default function Admin() {
-    return (
-        <div className="flex p-4 min-h-screen background">
-            <div className="mx-auto flex flex-col gap-4 max-w-[1300px] overflow-hidden">
-                <div className="bg-white flex flex-col rounded-lg p-3 w-full h-full lg:h-1/2">
-                    <div className="flex gap-4 items-center">
-                    <Link href="/admin/add-a-new-user" className="bg-blue-500 w-fit text-white px-4 py-2 rounded-full transition-colors hover:bg-blue-600">
-                        +
-                    </Link>
-                    <h2>Users</h2>
-                    </div>
-                    <div className="flex flex-col mt-4 overflow-x-auto">
-                        <UsersTable />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex p-4 min-h-screen background">
+      <div className="mx-auto flex flex-col gap-4 max-w-[1300px] overflow-hidden">
+        <div className="bg-white flex flex-col rounded-lg p-3 w-full h-full lg:h-1/2">
+          <div className="flex gap-4 items-center">
+            <Link
+              href="/admin/add-a-new-user"
+              className="bg-blue-500 w-fit text-white px-4 py-2 rounded-full transition-colors hover:bg-blue-600"
+            >
+              +
+            </Link>
+            <h2>Users</h2>
+          </div>
+          <div className="flex flex-col mt-4 overflow-x-auto">
+            <UsersTable />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
