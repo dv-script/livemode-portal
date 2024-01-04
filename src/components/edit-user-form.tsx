94ef8953756125c738e3a6a08eaf0f6093757b48
Davi Hasson Castro <davihasson@gmail.com>
2024-01-04 00:57:29 -0300

    feat: catching all errors and showing in the application

--- src/components/edit-user-form.tsx
+++ src/components/edit-user-form.tsx
@@ -8,6 +8,8 @@ import {
   Checkbox,
   Input,
 } from "@nextui-org/react";
+import { FormError } from "./form-error";
+import { useEffect } from "react";
 
 const allRoles = [
   { name: "B2B Portal", key: "b2b_portal" },
@@ -34,9 +36,14 @@ interface EditUserFormProps {
 }
 
 export function EditUserForm({onClose, user} : EditUserFormProps) {
-  const initialState = { message: null, errors: {} };
-  //@ts-ignore
+  const initialState = { message: '', errors: {} };
   const [state, dispatch] = useFormState(editUser, initialState);
+
+  useEffect(() => {
+    if (state.success) {
+      onClose();
+    }
+  }, [onClose, state.success]);
 
   return (
     <form action={dispatch}>
@@ -50,6 +57,11 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.email}
           name="email"
         />
+        {state.errors?.email && state.errors.email.map(error => (
+          <p aria-live="polite" key={error} className="text-xs text-red-500">
+            {error}
+          </p>
+        ))}
         <Input
           label="First Name"
           placeholder="Enter the first name of the user"
@@ -57,6 +69,11 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.first_name}
           name="firstName"
         />
+        {state.errors?.firstName && state.errors.firstName.map(error => (
+          <p aria-live="polite" key={error} className="text-xs text-red-500">
+            {error}
+          </p>
+        ))}
         <Input
           label="Last Name"
           placeholder="Enter the last name of the user"
@@ -64,6 +81,11 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.last_name}
           name="lastName"
         />
+        {state.errors?.lastName && state.errors.lastName.map(error => (
+          <p aria-live="polite" key={error} className="text-xs text-red-500">
+            {error}
+          </p>
+        ))}
         <Input
           label="Company"
           placeholder="Enter the company name"
@@ -71,6 +93,11 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.company}
           name="company"
         />
+        {state.errors?.company && state.errors.company.map(error => (
+          <p aria-live="polite" key={error} className="text-xs text-red-500">
+            {error}
+          </p>
+        ))}
         <ul className="flex flex-col gap-2 py-2 px-1">
           {allRoles.map((role) => (
             <li key={role.key}>
@@ -86,13 +113,19 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
               </Checkbox>
             </li>
           ))}
+          {state.errors?.roles && state.errors.roles.map(error => (
+          <p aria-live="polite" key={error} className="text-xs text-red-500">
+            {error}
+          </p>
+        ))}
         </ul>
       </ModalBody>
       <ModalFooter>
+        {state.success === false && <FormError errorMessage={state.message}/>}
         <Button type="reset" color="danger" variant="flat">
           Reset
         </Button>
-        <Button type="submit" color="primary" onPress={onClose}>
+        <Button type="submit" color="primary">
           Confirm
         </Button>
       </ModalFooter>
