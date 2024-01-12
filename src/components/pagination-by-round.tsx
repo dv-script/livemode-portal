97f38c834b9931b2780e6eb6a9cf375182b24545
Davi Hasson Castro <davihasson@gmail.com>
2024-01-12 00:06:53 -0300

    feat: adding a new pagination by match round

---
+++ src/components/pagination-by-round.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { useState } from "react";
+
+interface IPaginationByRoundProps {
+  maxRound: number;
+}
+
+export function PaginationByRound({ maxRound }: IPaginationByRoundProps) {
+  const [selectedRound, setSelectedRound] = useState(1);
+
+  function handleNextRound() {
+    if (selectedRound < maxRound) setSelectedRound((prev) => prev + 1);
+  }
+
+  function handlePreviousRound() {
+    if (selectedRound > 1) setSelectedRound((prev) => prev - 1);
+  }
+
+  return (
+    <div className="flex flex-row space- items-center gap-1 py-4">
+      <button
+        onClick={handlePreviousRound}
+        disabled={selectedRound === 1 && true}
+        className="border-none text-xl bg-transparent outline-none p-2 disabled:text-zinc-400"
+      >
+        <FaAngleLeft />
+      </button>
+      <span className="text-xl font-semibold">Rodada {selectedRound}</span>
+      <button
+        onClick={handleNextRound}
+        disabled={selectedRound === maxRound - 1 && true}
+        className="border-none text-xl bg-transparent outline-none p-2 disabled:text-zinc-400"
+      >
+        <FaAngleRight />
+      </button>
+    </div>
+  );
+}
