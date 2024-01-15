09155c4b92d14cec8e7ef7c4ce7da58acfb757fc
Davi Hasson Castro <davihasson@gmail.com>
2024-01-15 12:01:39 -0300

    feat: adding types to standings

---
+++ src/types/IFetchStandingsResponse.ts
@@ -0,0 +1,6 @@
+import { IStanding } from "./IStanding";
+
+export interface IFetchStandingsResponse {
+  data: IStanding[];
+  pagination: null;
+}
---
+++ src/types/IStanding.ts
@@ -0,0 +1,31 @@
+export interface IStanding {
+  awayDraws: number;
+  awayLosses: number;
+  awayWins: number;
+  championship: string;
+  cupName: string;
+  division: string;
+  draws: number;
+  efficiency: number;
+  gamesPlayed: number;
+  goalsAgainst: number;
+  goalsDifference: number;
+  goalsFor: number;
+  groupname: string;
+  homeDraws: number;
+  homeLosses: number;
+  homeWins: number;
+  id: number;
+  idChampionship: number;
+  idTeam: number;
+  losses: number;
+  maxPointsPossible: number;
+  phase: string;
+  points: number;
+  position: number;
+  redCards: number;
+  round: number;
+  team: string;
+  wins: number;
+  yellowCards: number;
+}
