ae760339772c184d76d28493479b689bea132241
Davi Hasson Castro <davihasson@gmail.com>
2024-01-15 20:02:30 -0300

    feat: new function to catch top scorers and players info

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,5 +1,5 @@
 import { unstable_noStore } from "next/cache";
-import { matchesURL, standingsURL, teamURL } from "./constants";
+import { matchesURL, playersURL, standingsURL, teamURL, topScorersURL } from "./constants";
 
 export async function fetchMatches() {
   unstable_noStore();
@@ -31,6 +31,32 @@ export async function fetchStandings() {
   unstable_noStore();
   try {
     const response = await fetch(standingsURL, {
+      headers: { Authorization: `Bearer ${process.env.FOOTSTATS_TOKEN}` },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.log("Error to get the standings", error);
+  }
+}
+
+export async function fetchPlayerById(id: number) {
+  unstable_noStore();
+  try {
+    const response = await fetch(playersURL.replace(":id", String(id)), {
+      headers: { Authorization: `Bearer ${process.env.FOOTSTATS_TOKEN}` },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.log(`Error trying to get the id from: ${id}`, error);
+  }
+}
+
+export async function fetchTopScorers() {
+  unstable_noStore();
+  try {
+    const response = await fetch(topScorersURL, {
       headers: { Authorization: `Bearer ${process.env.FOOTSTATS_TOKEN}` },
     });
 
