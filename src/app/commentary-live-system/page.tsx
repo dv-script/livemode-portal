63b0302adf8581180104b32750205edde664308b
Davi Hasson Castro <davihasson@gmail.com>
2024-01-15 11:57:16 -0300

    feat: adding standings table to page

--- src/app/commentary-live-system/page.tsx
+++ src/app/commentary-live-system/page.tsx
@@ -1,17 +1,25 @@
 import { fetchMatches } from "@/lib/fetcher";
 import { IFetchMatchesResponse } from "@/types/IFetchMatchesResponse";
-import { PaginationByRound } from "@/components/pagination-by-round";
 import { MatchesByRound } from "@/components/matches-by-round";
+import { PaginationByRound } from "@/components/pagination-by-round";
+import { StandingsTable } from "@/components/standings-table";
+
 
 export default async function Page() {
-  const allMatches = await fetchMatches() as IFetchMatchesResponse;
+  const allMatches = (await fetchMatches()) as IFetchMatchesResponse;
+  const maxRound = allMatches.data.slice(-1)[0].round;
 
   return (
     <main className="flex bg-gray-100">
-      <div className="flex flex-col gap-4 py-4 px-2 max-w-[1300px] mx-auto">
-        <PaginationByRound maxRound={allMatches.data.slice(-1)[0].rodada} />
-        <div className="flex flex-wrap gap-4">
-          <MatchesByRound allMatches={allMatches}/>
+      <div className="flex flex-col gap-4 py-4 px-2 max-w-[1300px] overflow-hidden mx-auto md:grid grid-cols-12">
+        <div className="col-span-8">
+          <StandingsTable />
+        </div>
+        <div className="col-span-4">
+          <PaginationByRound maxRound={maxRound} />
+          <div className="flex flex-wrap gap-4 py-4">
+            <MatchesByRound allMatches={allMatches} />
+          </div>
         </div>
       </div>
     </main>
