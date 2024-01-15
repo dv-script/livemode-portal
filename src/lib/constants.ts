a24adbe247f2cd6ec9b9cfc8e39cc743986bbf7f
Davi Hasson Castro <davihasson@gmail.com>
2024-01-15 20:02:11 -0300

    chore: adding players url and top scorer url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 export const matchesURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/matches';
 export const teamURL = 'http://apifutebol.footstats.com.br/3.1/teams/:id';
 export const standingsURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/standings';
+export const playersURL = 'http://apifutebol.footstats.com.br/3.1/players/:id';
+export const topScorersURL = 'http://apifutebol.footstats.com.br/3.1/championships/950/scorers';
