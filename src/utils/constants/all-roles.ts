90b58b519f8dd0ead18d505318c87a0062398d2a
Davi Hasson Castro <davihasson@gmail.com>
2024-01-16 16:26:15 -0300

    chore: adding all-roles as constants in utils

---
+++ src/utils/constants/all-roles.ts
@@ -0,0 +1,9 @@
+export const allRoles = [
+  { name: "B2B Portal", key: "b2b_portal" },
+  { name: "Photo Database", key: "photo_database" },
+  { name: "Commentary Live System", key: "commentary_live_system" },
+  { name: "Costumer Service Tool", key: "costumer_service_tool" },
+  { name: "Match Analysis Hub", key: "match_analysis_hub" },
+  { name: "Media Portal", key: "media_portal" },
+  { name: "Administrator", key: "admin" },
+];
