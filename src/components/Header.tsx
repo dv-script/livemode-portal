b11c12f79143ba63684eb0769f30f8d104a1f790
Davi Hasson Castro <davihasson@gmail.com>
2024-01-16 16:23:09 -0300

    refactor: adding a const that checks if is admin or not

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,50 @@
 import Link from "next/link";
 import { auth } from "@/app/auth/providers";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button} from "@nextui-org/react";
-import { UserProfile } from "./user-profile"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@nextui-org/react";
+import { UserProfile } from "./user-profile";
 
 export async function Header() {
-  const session = await auth()
+  const session = await auth();
+  const isAdmin = session?.user?.roles.includes("admin") ? true : false;
 
   return (
     <Navbar>
       <NavbarBrand>
-        <Link href="/" className="font-bold text-inherit">LIVEMODE</Link>
+        <Link href="/" className="font-bold text-inherit">
+          LIVEMODE
+        </Link>
       </NavbarBrand>
       <NavbarContent justify="end">
         {session?.user ? (
-            <UserProfile 
-              first_name={session?.user?.first_name}
-              last_name={session?.user?.last_name}
-              company={session?.user?.company}
-              isAdmin={session?.user?.roles.includes('admin') && true}
-            />
+          <UserProfile
+            firstName={session?.user?.firstName}
+            lastName={session?.user?.lastName}
+            company={session?.user?.company}
+            isAdmin={isAdmin}
+          />
         ) : (
           <>
             <NavbarItem className="hidden lg:flex">
-              <Link href="/auth/sign-in" className="transition hover:text-gray-500">Login</Link>
+              <Link
+                href="/auth/sign-in"
+                className="transition hover:text-gray-500"
+              >
+                Login
+              </Link>
             </NavbarItem>
             <NavbarItem>
-              <Button as={Link} color="primary" href="/auth/request-an-account" variant="flat">
+              <Button
+                as={Link}
+                color="primary"
+                href="/auth/request-an-account"
+                variant="flat"
+              >
                 Request an Account
               </Button>
             </NavbarItem>
@@ -35,4 +54,3 @@ export async function Header() {
     </Navbar>
   );
 }
-
