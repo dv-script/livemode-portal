a7869756e1e9a5b212a87448ff863e8daadb2446
Davi Hasson Castro <davihasson@gmail.com>
2024-01-16 16:22:04 -0300

    feat: adding select from nextui to form

--- src/components/edit-user-form.tsx
+++ src/components/edit-user-form.tsx
@@ -7,36 +7,39 @@ import {
   Button,
   Checkbox,
   Input,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 import { FormError } from "./form-error";
 import { useEffect } from "react";
+import { allRoles } from "@/utils/constants/all-roles";
+import { IUser } from "@/types/IUser";
 
-const allRoles = [
-  { name: "B2B Portal", key: "b2b_portal" },
-  { name: "Photo Database", key: "photo_database" },
-  { name: "Commentary Live System", key: "commentary_live_system" },
-  { name: "Costumer Service Tool", key: "costumer_service_tool" },
-  { name: "Match Analysis Hub", key: "match_analysis_hub" },
-  { name: "Media Portal", key: "media_portal" },
-  { name: "Administrator", key: "admin" },
+const status = [
+  {
+    key: "active",
+    color: "bg-blue-500",
+    label: "Active",
+  },
+  {
+    key: "inactive",
+    color: "bg-red-500",
+    label: "Inactive",
+  },
+  {
+    key: "paused",
+    color: "bg-yellow-500",
+    label: "Paused",
+  },
 ];
 
 interface EditUserFormProps {
-  onClose: () => void
-    user: {
-      id: string
-      email: string
-      first_name: string
-      last_name: string
-      company: string
-      roles: string[]
-      created_at: Date
-      updated_at: Date
-    }
+  onClose: () => void;
+  user: IUser;
 }
 
-export function EditUserForm({onClose, user} : EditUserFormProps) {
-  const initialState = { message: '', errors: {} };
+export function EditUserForm({ onClose, user }: EditUserFormProps) {
+  const initialState = { message: "", errors: {} };
   const [state, dispatch] = useFormState(editUser, initialState);
 
   useEffect(() => {
@@ -57,35 +60,80 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.email}
           name="email"
         />
-        {state.errors?.email && state.errors.email.map(error => (
-          <p aria-live="polite" key={error} className="text-xs text-red-500">
-            {error}
-          </p>
-        ))}
+        {state.errors?.email &&
+          state.errors.email.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
         <Input
           label="First Name"
           placeholder="Enter the first name of the user"
           variant="bordered"
-          defaultValue={user.first_name}
+          defaultValue={user.firstName}
           name="firstName"
         />
-        {state.errors?.firstName && state.errors.firstName.map(error => (
-          <p aria-live="polite" key={error} className="text-xs text-red-500">
-            {error}
-          </p>
-        ))}
+        {state.errors?.firstName &&
+          state.errors.firstName.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
         <Input
           label="Last Name"
           placeholder="Enter the last name of the user"
           variant="bordered"
-          defaultValue={user.last_name}
+          defaultValue={user.lastName}
           name="lastName"
         />
-        {state.errors?.lastName && state.errors.lastName.map(error => (
-          <p aria-live="polite" key={error} className="text-xs text-red-500">
-            {error}
-          </p>
-        ))}
+        {state.errors?.lastName &&
+          state.errors.lastName.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
+
+        <Input
+          label="Phone Number"
+          placeholder="Enter the phone number of the user"
+          variant="bordered"
+          defaultValue={user.phoneNumber}
+          name="phoneNumber"
+        />
+        {state.errors?.phoneNumber &&
+          state.errors.phoneNumber.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
+
+        <Select
+          label="Status"
+          placeholder="Select the status of the user"
+          defaultSelectedKeys={[user.status]}
+          name="status"
+        >
+          {status.map((item) => (
+            <SelectItem
+              key={item.key}
+              startContent={
+                <span
+                  className={`inline-block w-2 h-2 rounded-full ${item.color}`}
+                ></span>
+              }
+            >
+              {item.label}
+            </SelectItem>
+          ))}
+        </Select>
+
+        {state.errors?.status &&
+          state.errors.status.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
+
         <Input
           label="Company"
           placeholder="Enter the company name"
@@ -93,11 +141,12 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
           defaultValue={user.company}
           name="company"
         />
-        {state.errors?.company && state.errors.company.map(error => (
-          <p aria-live="polite" key={error} className="text-xs text-red-500">
-            {error}
-          </p>
-        ))}
+        {state.errors?.company &&
+          state.errors.company.map((error) => (
+            <p aria-live="polite" key={error} className="text-xs text-red-500">
+              {error}
+            </p>
+          ))}
         <ul className="flex flex-col gap-2 py-2 px-1">
           {allRoles.map((role) => (
             <li key={role.key}>
@@ -113,15 +162,20 @@ export function EditUserForm({onClose, user} : EditUserFormProps) {
               </Checkbox>
             </li>
           ))}
-          {state.errors?.roles && state.errors.roles.map(error => (
-          <p aria-live="polite" key={error} className="text-xs text-red-500">
-            {error}
-          </p>
-        ))}
+          {state.errors?.roles &&
+            state.errors.roles.map((error) => (
+              <p
+                aria-live="polite"
+                key={error}
+                className="text-xs text-red-500"
+              >
+                {error}
+              </p>
+            ))}
         </ul>
       </ModalBody>
       <ModalFooter>
-        {state.success === false && <FormError errorMessage={state.message}/>}
+        {state.success === false && <FormError errorMessage={state.message} />}
         <Button type="reset" color="danger" variant="flat">
           Reset
         </Button>
