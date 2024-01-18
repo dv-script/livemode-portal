8e19af863555f984f025a801ec6c8fd6c7f900f5
Davi Hasson Castro <davihasson@gmail.com>
2024-01-18 18:43:57 -0300

    chore: protecting routes from roles and add to session a expires from token jwt in 1 day

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -10,9 +10,36 @@ export const authConfig = {
       const isLogIn = !!auth?.user;
       const isAuthRoutes = nextUrl.pathname.startsWith("/auth");
       const isPrivateRoutes = !isAuthRoutes && nextUrl.pathname.startsWith("/");
+      const statusActive = auth?.user?.status === "active" ? true : false;
 
-      const isAdmin = auth?.user?.roles?.includes("admin");
+      const hasAdminAccess = auth?.user?.roles?.includes("admin");
+      const hasB2bPortalAccess = auth?.user?.roles?.includes("b2b-portal");
+      const hasPhotoDatabaseAccess =
+        auth?.user?.roles?.includes("photo-database");
+      const hasCommentaryLiveSystemAccess = auth?.user?.roles?.includes(
+        "commentary-live-system"
+      );
+      const hasCostumerServiceToolAccess = auth?.user?.roles?.includes(
+        "costumer-service-tool"
+      );
+      const hasMatchAnalysisHubAccess =
+        auth?.user?.roles?.includes("match-analysis-hub");
+      const hasMediaPortalAccess = auth?.user?.roles?.includes("media-portal");
+
       const isAdminRoutes = nextUrl.pathname.startsWith("/admin");
+      const isB2bRoutes = nextUrl.pathname.startsWith("/b2b-portal");
+      const isPhotoDatabaseRoutes =
+        nextUrl.pathname.startsWith("/photo-database");
+      const isCommentaryLiveSystemRoutes = nextUrl.pathname.startsWith(
+        "/commentary-live-system"
+      );
+      const isCostumerServiceToolRoutes = nextUrl.pathname.startsWith(
+        "/costumer-service-tool"
+      );
+      const isMatchAnalysisHubRoutes = nextUrl.pathname.startsWith(
+        "/match-analysis-hub"
+      );
+      const isMediaPortalRoutes = nextUrl.pathname.startsWith("/media-portal");
 
       if (!isLogIn && isPrivateRoutes) {
         return false;
@@ -22,14 +49,54 @@ export const authConfig = {
         return Response.redirect(new URL("/", nextUrl));
       }
 
-      if (isAdminRoutes && !isAdmin) {
+      if (isAdminRoutes && !hasAdminAccess && !statusActive) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (isB2bRoutes && !hasB2bPortalAccess && !statusActive) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (isPhotoDatabaseRoutes && !hasPhotoDatabaseAccess && !statusActive) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (
+        isCommentaryLiveSystemRoutes &&
+        !hasCommentaryLiveSystemAccess &&
+        !statusActive
+      ) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (
+        isCostumerServiceToolRoutes &&
+        !hasCostumerServiceToolAccess &&
+        !statusActive
+      ) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (
+        isMatchAnalysisHubRoutes &&
+        !hasMatchAnalysisHubAccess &&
+        !statusActive
+      ) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (isMediaPortalRoutes && !hasMediaPortalAccess && !statusActive) {
         return Response.redirect(new URL("/", nextUrl));
       }
 
       return true;
     },
     jwt({ token, user }) {
+      const expireIn = 60 * 60 * 24;
+      const expirationDate = new Date(Date.now() + expireIn);
+
       if (user) {
+        token.expires = expirationDate.toISOString();
         token.status = user.status;
         token.roles = user.roles;
         token.company = user.company;
@@ -39,6 +106,7 @@ export const authConfig = {
       return token;
     },
     session({ session, token }) {
+      if (token.expires) session.expires = token.expires;
       if (token.roles) session.user.roles = token.roles;
       if (token.company) session.user.company = token.company;
       if (token.firstName) session.user.firstName = token.firstName;
