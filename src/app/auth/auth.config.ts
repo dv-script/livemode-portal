ed861a01bb1d53cee37442502a58b5e4ae2c34c4
Davi Hasson Castro <davihasson@gmail.com>
2024-01-18 19:08:13 -0300

    chore: removing wrong config

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -7,82 +7,18 @@ export const authConfig = {
   },
   callbacks: {
     authorized: ({ auth, request: { nextUrl } }) => {
-      const isLogIn = !!auth?.user;
+      const isLoggedIn = !!auth?.user;
       const isAuthRoutes = nextUrl.pathname.startsWith("/auth");
       const isPrivateRoutes = !isAuthRoutes && nextUrl.pathname.startsWith("/");
-      const statusActive = auth?.user?.status === "active" ? true : false;
-
+      const isAdminRoutes =
+        isPrivateRoutes && nextUrl.pathname.startsWith("/admin");
       const hasAdminAccess = auth?.user?.roles?.includes("admin");
-      const hasB2bPortalAccess = auth?.user?.roles?.includes("b2b-portal");
-      const hasPhotoDatabaseAccess =
-        auth?.user?.roles?.includes("photo-database");
-      const hasCommentaryLiveSystemAccess = auth?.user?.roles?.includes(
-        "commentary-live-system"
-      );
-      const hasCostumerServiceToolAccess = auth?.user?.roles?.includes(
-        "costumer-service-tool"
-      );
-      const hasMatchAnalysisHubAccess =
-        auth?.user?.roles?.includes("match-analysis-hub");
-      const hasMediaPortalAccess = auth?.user?.roles?.includes("media-portal");
 
-      const isAdminRoutes = nextUrl.pathname.startsWith("/admin");
-      const isB2bRoutes = nextUrl.pathname.startsWith("/b2b-portal");
-      const isPhotoDatabaseRoutes =
-        nextUrl.pathname.startsWith("/photo-database");
-      const isCommentaryLiveSystemRoutes = nextUrl.pathname.startsWith(
-        "/commentary-live-system"
-      );
-      const isCostumerServiceToolRoutes = nextUrl.pathname.startsWith(
-        "/costumer-service-tool"
-      );
-      const isMatchAnalysisHubRoutes = nextUrl.pathname.startsWith(
-        "/match-analysis-hub"
-      );
-      const isMediaPortalRoutes = nextUrl.pathname.startsWith("/media-portal");
-
-      if (!isLogIn && isPrivateRoutes) {
-        return false;
-      }
-
-      if (isLogIn && isAuthRoutes) {
+      if (isLoggedIn && isAuthRoutes) {
         return Response.redirect(new URL("/", nextUrl));
       }
 
-      if (isAdminRoutes && (!hasAdminAccess || !statusActive)) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (isB2bRoutes && (!hasB2bPortalAccess || !statusActive)) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (isPhotoDatabaseRoutes && (!hasPhotoDatabaseAccess || !statusActive)) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (
-        isCommentaryLiveSystemRoutes &&
-        (!hasCommentaryLiveSystemAccess || !statusActive)
-      ) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (
-        (isCostumerServiceToolRoutes && !hasCostumerServiceToolAccess) ||
-        !statusActive
-      ) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (
-        isMatchAnalysisHubRoutes &&
-        (!hasMatchAnalysisHubAccess || !statusActive)
-      ) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-
-      if (isMediaPortalRoutes && (!hasMediaPortalAccess || !statusActive)) {
+      if (isAdminRoutes && !hasAdminAccess) {
         return Response.redirect(new URL("/", nextUrl));
       }
 
