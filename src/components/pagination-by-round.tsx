84e1852bf94384cd3c3f2cdd01ff4b0654975fd8
Davi Hasson Castro <davihasson@gmail.com>
2024-01-19 05:51:44 -0300

    feat: adding to ordinal function to transform round to ordinal

--- src/components/pagination-by-round.tsx
+++ src/components/pagination-by-round.tsx
@@ -1,35 +1,36 @@
 "use client";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { useState } from "react";
+import { toOrdinal } from "@/utils/toOrdinal";
 
 interface IPaginationByRoundProps {
   maxRound: number | null;
 }
 
 export function PaginationByRound({ maxRound }: IPaginationByRoundProps) {
-  const [selectedRound, setSelectedRound] = useState(1);
+  const [currentRound, setCurrentRound] = useState(1);
 
   function handleNextRound() {
-    if (maxRound && selectedRound < maxRound) setSelectedRound((prev) => prev + 1);
+    if (maxRound && currentRound < maxRound) setCurrentRound((prev) => prev + 1);
   }
 
   function handlePreviousRound() {
-    if (selectedRound > 1) setSelectedRound((prev) => prev - 1);
+    if (currentRound > 1) setCurrentRound((prev) => prev - 1);
   }
 
   return (
-    <div className="flex flex-row items-center gap-1">
+    <div className="flex flex-row items-center gap-1 justify-between">
       <button
         onClick={handlePreviousRound}
-        disabled={selectedRound === 1 && true}
+        disabled={currentRound === 1 && true}
         className="border-none text-xl bg-transparent outline-none p-2 disabled:text-zinc-400"
       >
         <FaAngleLeft />
       </button>
-      <span className="text-xl font-semibold">{selectedRound} Round</span>
+      <span className="text-xl font-semibold">{toOrdinal(currentRound)} Round</span>
       <button
         onClick={handleNextRound}
-        disabled={maxRound ? selectedRound === maxRound && true : true}
+        disabled={maxRound ? currentRound === maxRound && true : true}
         className="border-none text-xl bg-transparent outline-none p-2 disabled:text-zinc-400"
       >
         <FaAngleRight />
