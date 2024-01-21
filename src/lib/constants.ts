5192e9deb97633fea868530b554c2b6205189e62
Davi Hasson Castro <davihasson@gmail.com>
2024-01-21 15:19:15 -0300

    chore: changing the championship form topScorersURL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ export const matchesURL = 'http://apifutebol.footstats.com.br/3.1/championships/
 export const teamURL = 'http://apifutebol.footstats.com.br/3.1/teams/:id';
 export const standingsURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/standings';
 export const playersURL = 'http://apifutebol.footstats.com.br/3.1/players/:id';
-export const topScorersURL = 'http://apifutebol.footstats.com.br/3.1/championships/950/scorers';
+export const topScorersURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/scorers';
