e4b648702f21ca34da6e5ee2eb4790b17b62504c
Davi Hasson Castro <davihasson@gmail.com>
2024-02-06 14:17:01 -0300

    style: linting the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import { Card } from "@/components/service-card";
 import { servicesInfoMap } from "@/enum/services";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen background">
-            <div className="flex gap-4 justify-center items-center flex-wrap px-4 py-28 m-auto max-w-[1300px]">
-                {Object.values(servicesInfoMap).map((serviceInfo) => (
-                    <Card
-                        key={serviceInfo.id}
-                        title={serviceInfo.title}
-                        small_description={serviceInfo.small_description}
-                        description_title={serviceInfo.description_title}
-                        description={serviceInfo.description}
-                        image={serviceInfo.image}
-                        link={serviceInfo.link}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="min-h-screen background">
+      <div className="flex gap-4 justify-center items-center flex-wrap px-4 py-28 m-auto max-w-[1300px]">
+        {Object.values(servicesInfoMap).map((serviceInfo) => (
+          <Card
+            key={serviceInfo.id}
+            title={serviceInfo.title}
+            small_description={serviceInfo.small_description}
+            description_title={serviceInfo.description_title}
+            description={serviceInfo.description}
+            image={serviceInfo.image}
+            link={serviceInfo.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
