8aff408d91d4b5efd6c387e2567c111d1ae03a5b
Davi Hasson Castro <davihasson@gmail.com>
2024-02-06 14:20:53 -0300

    refactor: moving constants to outside from utils

--- src/constants/constants-url.ts
+++ src/constants/constants-url.ts
@@ -3,3 +3,5 @@ export const teamURL = 'http://apifutebol.footstats.com.br/3.1/teams/:id';
 export const standingsURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/standings';
 export const playersURL = 'http://apifutebol.footstats.com.br/3.1/players/:id';
 export const topScorersURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/scorers';
+export const scoutsByMatchURL = 'http://apifutebol.footstats.com.br/3.1/matches/:id/teams/scout';
+export const allScoutsURL = 'http://apifutebol.footstats.com.br/3.1/championships/935/teams/scout';
